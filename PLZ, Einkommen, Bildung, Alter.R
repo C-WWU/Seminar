73f20a265266eff6ba11fa7c8a8d610cd4cb3c76
C-WWU <christoph_barg@web.de>
2021-01-14 11:28:15 +0100

    Ergänzt um drug, alk, zig

--- PLZ, Einkommen, Bildung, Alter.R
+++ PLZ, Einkommen, Bildung, Alter.R
@@ -17,8 +17,29 @@ Bildung_test <- data %>% mutate(Bildungsgruppe = case_when(Bildungsabschluss == 
 
 ###machen die Einkommengruppen Sinn? die erste Zeile funktioniert nicht -.-
 Einkommen_test <- data %>% mutate(Einkommensgruppe = case_when(Nettoeinkommen == "0 € - 1000 €" ~ 'niedriges Einkommen',
-                                                               Nettoeinkommen == "1001 € - 2000 €" ~ 'niedriges Einkommen',
+                                                               Nettoeinkommen == "1001 € - 2000€" ~ 'niedriges Einkommen',
                                                                Nettoeinkommen == "2001 € - 3000 €" ~ 'mittleres Einkommen',
                                                                Nettoeinkommen == "3001 € - 4000 €" ~ 'mittleres Einkommen',
                                                                Nettoeinkommen == "4001 € - 5000 €" ~ 'hohes Einkommen',
                                                                Nettoeinkommen == "Mehr als 5000 €" ~ 'hohes Einkommen'))
+
+Alkohol_test <- data %>% mutate(Alkoholgruppe = case_when(`Alkohol Konsum` == "Nein" ~ 'kein Konsum',
+                                                          `Alkohol Konsum` == "Ja, mindestens einmal im Jahr" ~ 'niedriger Konsum',
+                                                          `Alkohol Konsum` == "Ja, mindestens einmal im Monat" ~ 'niedriger Konsum',
+                                                          `Alkohol Konsum` == "Ja, mindestens einmal pro Woche" ~ 'hoher Konsum',
+                                                          `Alkohol Konsum` == "Ja, mehrmals pro Woche" ~ 'hoher Konsum',
+                                                          `Alkohol Konsum` == "Ja, täglich" ~ 'hoher Konsum'))
+
+Zigaretten_test <- data %>% mutate(Zigarettengruppe = case_when(`Zigaretten Konsum` == "Nein" ~ 'kein Konsum',
+                                                          `Zigaretten Konsum` == "Ja, mindestens einmal im Jahr" ~ 'niedriger Konsum',
+                                                          `Zigaretten Konsum` == "Ja, mindestens einmal im Monat" ~ 'niedriger Konsum',
+                                                          `Zigaretten Konsum` == "Ja, mindestens einmal pro Woche" ~ 'hoher Konsum',
+                                                          `Zigaretten Konsum` == "Ja, mehrmals pro Woche" ~ 'hoher Konsum',
+                                                          `Zigaretten Konsum` == "Ja, täglich" ~ 'hoher Konsum'))
+
+Drogen_test <- data %>% mutate(Drogengruppe = case_when(`Drogen Konsum` == "Nein" ~ 'kein Konsum',
+                                                        `Drogen Konsum` == "Ja, mindestens einmal im Jahr" ~ 'niedriger Konsum',
+                                                        `Drogen Konsum` == "Ja, mindestens einmal im Monat" ~ 'niedriger Konsum',
+                                                        `Drogen Konsum` == "Ja, mindestens einmal pro Woche" ~ 'hoher Konsum',
+                                                        `Drogen Konsum` == "Ja, mehrmals pro Woche" ~ 'hoher Konsum',
+                                                        `Drogen Konsum` == "Ja, täglich" ~ 'hoher Konsum'))
